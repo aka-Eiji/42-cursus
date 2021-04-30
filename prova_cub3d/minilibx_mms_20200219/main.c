78afa55e1f958f3d126e4481eca9fd7e1307dd28
Joseph Kosiara <jkosiara@e4r10p1.42roma.it>
2021-04-30 18:14:56 +0200

    new modifies

--- prova_cub3d/minilibx_mms_20200219/main.c
+++ prova_cub3d/minilibx_mms_20200219/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmurello <mmurello@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkosiara <jkosiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 18:20:42 by jkosiara          #+#    #+#             */
-/*   Updated: 2021/04/27 19:15:02 by mmurello         ###   ########.fr       */
+/*   Updated: 2021/04/30 18:14:13 by jkosiara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
-int             close(int keycode, t_vars *vars)
+int             ft_close(int keycode, t_vars *vars)
 {
     /*mlx_destroy_window(vars->mlx, vars->win);*/
     printf("Game closed, thanks for playing!\n");
@@ -68,14 +68,40 @@ int             key_hook(int keycode, t_vars *vars)
          printf("You pressed D button, nice!\n");
 }
 
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	negative;
+
+	negative = 1;
+	res = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+	return (res * negative);
+}
 int             main(int argc, char **argv)
 {
     t_vars      vars;
     t_data      img;
-    int         i;
-    int         j;
+    char        *tmp1;
+    char        *tmp2;
+    int         res1;
+    int         res2;
     
-    i = 0;
+    tmp1 = argv[1];
+    tmp2 = argv[2];
+    res1 = ft_atoi(tmp1);
+    res2 = ft_atoi(tmp2);
     vars.mlx = mlx_init();
     if (argc != 3)
     {
@@ -84,9 +110,9 @@ int             main(int argc, char **argv)
     }
     else
     {
-        vars.win = mlx_new_window(vars.mlx, 1920, 1080,  "Il gioco dell'anno!");
+        vars.win = mlx_new_window(vars.mlx, res1, res2,  "Il gioco dell'anno!");
     }
     mlx_key_hook(vars.win, key_hook, &vars);
-    mlx_hook(vars.win, 17, 1L<<0, close, &vars);
+    mlx_hook(vars.win, 17, 1L<<0, ft_close, &vars);
     mlx_loop(vars.mlx);
 } 
