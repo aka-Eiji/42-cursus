2133c8c927d9bce7f466a6fe32cf8f484e31b0ea
Michela Murello <mmurello@e3r2p5.42roma.it>
2021-05-03 17:42:14 +0200

    new

--- prova_cub3d/cub3d.h
+++ prova_cub3d/cub3d.h
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkosiara <jkosiara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmurello <mmurello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 18:09:00 by jkosiara          #+#    #+#             */
-/*   Updated: 2021/04/30 17:41:13 by jkosiara         ###   ########.fr       */
+/*   Updated: 2021/05/03 16:41:05 by mmurello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "mlx/mlx.h"
+
 typedef struct  s_vars 
 {
     void    *mlx;
@@ -19,24 +25,21 @@ typedef struct  s_vars
 
 typedef struct s_maps
 {
-    int resx;
-	int resy;
-	char *NO;
-	char *SO;
-	char *EA;
-	char *WE;
+    int		resx;
+	int		resy;
+	char	*NO;
+	char	*SO;
+	char	*EA;
+	char	*WE;
+	char	*S;
+	int		red;
+	int		green;
+	int		blue;
 }              t_maps;
 
-/*typedef struct s_pos{
-    
-}              t_pos;
-
-typedef struct s_all{
-	t_vars;
-	t_maps;
-	t_coord;
-}              t_all;
-*/
+// typedef struct s_pos{
+//    
+// }              t_pos;
 
 // typedef struct s_list
 // {
@@ -44,8 +47,19 @@ typedef struct s_all{
 // 	struct s_list	*next;
 // }				t_list;
 
-// typedef struct s_coord
-// {
+// typedef struct s_text
+// { 
+//	int *n;
+//	int *s;
+//	int *i;
+//  int *f;
+//  int *c;
+//  int *e;
+//  int *w;
+//	}
+
+ typedef struct s_coord
+ {
 // 	double		posx;
 // 	double		posy;
 // 	double		dirx;
@@ -56,6 +70,10 @@ typedef struct s_all{
 // 	double		raydiry;
 // 	int			mapx;					// casella mappa
 // 	int			mapy;
+//	char		**mtx;
+int	mapx;					// quantita' caselle orizz
+int	mapy;					// quantita' caselle vert
+char **mtx;					// caselle effettive mtx[i][j]
 // 	double 		deltadistx; 			// distanza che il raggio deve percorrere per andare da 1 lato x al successivo lato x, viceversa
 // 	double 		deltadisty;
 // 	int 		stepx;					//controllo delle componenti
@@ -63,4 +81,10 @@ typedef struct s_all{
 // 	double 		sidedistx;				//distanza dalla posizione iniziale del raggio al primo lato o destro o sinistro, dip dalla componente del raggio
 // 	double 		sidedisty;
 // 	double 		perpwalldist;			//verrà utilizzata per calcolare la lunghezza del raggio
-// }				t_coord;
+}				t_coord;
+
+typedef struct s_all{
+	t_vars vars;
+	t_maps maps; 					// mancano i nomi
+	t_coord coord;
+}              t_all;
--- prova_cub3d/include/maps/parsingmap.c
+++ prova_cub3d/include/maps/parsingmap.c
@@ -3,19 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parsingmap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkosiara <jkosiara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmurello <mmurello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 16:09:08 by jkosiara          #+#    #+#             */
-/*   Updated: 2021/04/30 17:55:34 by jkosiara         ###   ########.fr       */
+/*   Updated: 2021/05/03 17:41:19 by mmurello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "../cub3d.h"
-#include <stdio.h>
+#include "../../cub3d.h"
 
 char *charjoin(char *s, char c, int max)
 {
@@ -53,6 +48,30 @@ int gnl(int fd, char **line)
 	
 }
 
+void		ft_colors(char *newline, t_maps *maps)
+{
+	char	*tmp;
+	int		i;
+	int		len;
+
+	i = 0;
+	tmp = ft_strtrim(newline, "F \t");
+	len = ft_strlen(tmp);
+	while(i < len)
+	{
+		if (tmp[i] == ',')
+		{
+			i++;
+			break;
+		}
+		while (tmp[i] >= '0' && tmp[i] <= '9')
+		{
+			maps->red = (maps->red * 10) + (tmp[i] - 48);
+			i++;
+		}
+		i++;
+	}
+}
 void	ft_res(char *newline, t_maps *maps)
 {
 	char *tmp;
@@ -66,7 +85,7 @@ void	ft_res(char *newline, t_maps *maps)
     {
     	maps->resx = (maps->resx * 10) + (tmp[i] - 48);
         i++;
-    }
+    } //manca l'impostazione della risoluzione massima fissata a (2560 x 1440) anche se maps->resx > 2560 || maps-> resy > 1440
 	while(tmp[i] == '\t' || tmp[i] == ' ')
 		i++;
 	while(tmp[i] >= '0' && tmp[i] <= '9')
@@ -77,37 +96,37 @@ void	ft_res(char *newline, t_maps *maps)
 	free(tmp);
 }
 
-void	ft_textures(t_maps *maps, char *newline)
+void	ft_textures(t_maps *maps, char *newline) 
 {
 	char *tmp;
 	int i;
 
 	i = 0;
-	if(newline[i] == 'N')
+	if(newline[i] == 'N')									//Lettura texture Nord
 	{
 		tmp = ft_strtrim(newline, "NO\t  ");
 		maps->NO = tmp;
 	}
-	else if(newline[i] == 'S' && newline[1] == 'O')
+	else if(newline[i] == 'S' && newline[1] == 'O')			//Lettura texture Sud
 	{
 		tmp = ft_strtrim(newline, "SO\t ");
 		maps->SO = tmp;
 	}
-	else if(newline[i] == 'E' && newline[1] == 'A')
+	else if(newline[i] == 'E' && newline[1] == 'A')			//Lettura texture Est
 	{
 		tmp = ft_strtrim(newline, "EA\t ");
 		maps->EA = tmp;
 	}
-	else if(newline[i] == 'W' && newline[1] == 'E')
+	else if(newline[i] == 'W' && newline[1] == 'E')			//Lettura texture Ovest
 	{
 		tmp = ft_strtrim(newline, "WE\t ");
 		maps->WE = tmp;
 	}
-	/*else if(newline[i] == 'S')
+	else if(newline[i] == 'S')								//Lettura texture Sprite
 	{
 		tmp = ft_strtrim(newline, "S\t ");
 		maps->S = tmp;
-	}	*/
+	}
 }
 
 void	ft_parsemap(t_maps *maps, char *newline)
@@ -115,6 +134,7 @@ void	ft_parsemap(t_maps *maps, char *newline)
     int i;
     int fd;
 	char *tmp1;
+	char *tmp;
 	
 	i = 0;
 
@@ -122,10 +142,12 @@ void	ft_parsemap(t_maps *maps, char *newline)
     while (gnl(fd, &newline))
     {
 		tmp1 = newline;
-		if (tmp1[i] == 'R')
-			ft_res(tmp1, maps);
+		tmp = tmp1;
+	
+	if (tmp1[i] == 'R')
+			ft_res(tmp1, maps); 
 		else if (tmp1[i] == 'N')
-			ft_textures(maps, tmp1);
+			ft_textures(maps, tmp1); // mancano le texture specifiche
 		else if (tmp1[i] == 'S')
 		 	ft_textures(maps, tmp1);
 		else if (tmp1[i] == 'E')
@@ -134,16 +156,42 @@ void	ft_parsemap(t_maps *maps, char *newline)
 		 	ft_textures(maps, tmp1);
 		else if (tmp1[i] == 'S')
 		 	ft_textures(maps, tmp1);
+		else if (tmp1[i] == 'F')
+			ft_colors(tmp1, maps);
     }
 }
 
+int	ft_validmap(t_all *all)
+{
+	int i;
+	int j;
 
-int main()
+	i = -1;
+	while(++i < all->coord.mapy)
+	{
+		j = -1;
+		while(++j < all->coord.mapx)
+		{
+			if (all->coord.mtx[i][j] != '1' && i == 0) // sopra
+				return (-1);
+			else if (all->coord.mtx[i][j] != '1' && i == all->coord.mapy - 1) // dx
+				return (-1);
+			else if (all->coord.mtx[i][j] != '1' && j == 0) // sx
+				return (-1);
+			else if (all->coord.mtx[i][j] != '1' && j == all->coord.mapx - 1) // sotto
+				return (-1);
+		}
+	}
+	return (1);
+}
+
+int main(int argc, char **argv)
 {
 	t_maps maps;
 	char *newline;
-	
+
 	ft_parsemap(&maps, newline);
-	printf("resx %d\nresy %d\n NO %s\n SO %s\n EA %s\n WE %s\n", maps.resx, maps.resy, maps.NO, maps.SO, maps.EA, maps.WE);
+	//printf("resx %d\nresy %d\n NO %s\n SO %s\n EA %s\n WE %s\n S %s\n", maps.resx, maps.resy, maps.NO, maps.SO, maps.EA, maps.WE, maps.S);
+	printf("red %d\n", maps.red);
 	return 0;
 }
