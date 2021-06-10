53e13af705b4f334ae608afa375eef2e12bf2308
Joseph Kosiara <jkosiara@e3r2p1.42roma.it>
2021-06-10 17:19:13 +0200

    bonus in progress

--- minitalk/bonus/client_bonus.c
+++ minitalk/bonus/client_bonus.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkosiara <jkosiara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/08 17:27:40 by jkosiara          #+#    #+#             */
-/*   Updated: 2021/06/10 17:12:43 by jkosiara         ###   ########.fr       */
+/*   Created: 2021/06/10 17:07:56 by jkosiara          #+#    #+#             */
+/*   Updated: 2021/06/10 17:16:44 by jkosiara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "minitalk_bonus.h"
 
 char *ft_eightbit(char character)
 {
     int i;
     char *result;
 
-    result = malloc(sizeof(8));
+    result = malloc(sizeof(9));
     if (result == 0)
         exit(0);
-    result[7] = 0;
-    i = 6;
+    result[8] = 0;
+    i = 7;
     while (character > 1)
     {
         result[i] = (character % 2) + 48;
@@ -37,30 +37,42 @@ char *ft_eightbit(char character)
     }
     return (result);
 }
+char	*ft_eightbit_pluse(char cara)
+{
+	char	*string;
+
+	cara *= -1;
+	string = ft_ctobit(cara);
+	ft_string_complement(string);
+	return (string);
+}
 
 void ft_send_term(int pid)
 {
     int i;
     
     i = 0;
-    while (i < 7)
+    while (i < 8)
     {
         kill(pid,SIGUSR1);
         i++;
-        usleep(10);
+        usleep(50);
     }
 }
 
 void ft_send_signal(int pid, char *string)
 {
-    int     i;
-    int     j;
+    int i;
+    int j;
     char *temp;
-
+    
     i = 0;
     while(string[i])
     {
-        temp = ft_eightbit(string[i]);
+        if (string[i] < 0)
+            temp = ft_eightbit_pluse(string[i]);
+        else
+            temp = ft_eightbit(string[i]);
         j = 0;
         while(temp[j])
         {
@@ -69,23 +81,24 @@ void ft_send_signal(int pid, char *string)
             else
                 kill(pid,SIGUSR2);
             j++;
-            usleep(10);
+            usleep(50);
         }
         i++;
+        free(temp);
     }
     ft_send_term(pid);
 }
 
-int main (int argc, char **argv) 
+int	main(int argc, char *argv[])
 {
-    char *string;
-    int i;
-    int pid;
-    
-    ft_check_error(argc, argv);
-    i = 0;
-    pid = ft_atoi(argv[1]);
-    printf("%d\n", pid);
-    string = argv[2];
-    ft_send_signal(pid, string);
+	int		pid;
+	int		i;
+	char	*string;
+
+	if (argc != 3)
+		exit (0);
+	i = 0;
+	pid = ft_atoi(argv[1]);
+	string = argv[2];
+	ft_send_signal(pid, string);
 }
