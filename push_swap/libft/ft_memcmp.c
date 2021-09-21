5e664233fb693970bb6b44f31fd0420b9d7e7a56
Joseph Kosiara <jkosiara@e3r6p5.42roma.it>
2021-09-21 16:02:37 +0200

    wip

---
+++ push_swap/libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkosiara <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/22 16:40:34 by jkosiara          #+#    #+#             */
+/*   Updated: 2021/01/22 16:42:30 by jkosiara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned const char *str1;
+	unsigned const char *str2;
+
+	str1 = (unsigned const char *)s1;
+	str2 = (unsigned const char *)s2;
+	while (n)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+		n--;
+	}
+	return (0);
+}
